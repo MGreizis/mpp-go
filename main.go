79a861ff75d860e540ef304d5f36c00af971ae8f
MGreizis <maartinsgreizis@gmail.com>
2024-09-12 12:29:05 +0200

    assignment 1

--- main.go
+++ main.go
@@ -1,9 +1,102 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"log"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+const dbPath = "./data/movies.db"
+
+func openDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func addMovie(db *sql.DB, imdbID, title string, year int, rating float64) error {
+	stmt, err := db.Prepare("INSERT INTO movies (IMDb_id, Title, Year, Rating) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(imdbID, title, year, rating)
+	return err
+}
+
+func listMovies(db *sql.DB) error {
+	rows, err := db.Query("SELECT Title FROM movies")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var title string
+		if err := rows.Scan(&title); err != nil {
+			return err
+		}
+		fmt.Println(title)
+	}
+	return nil
+}
+
+func showMovieDetails(db *sql.DB, imdbID string) error {
+	var imdb_id, title string
+	var year int
+	var rating float64
+	var poster NullString
+
+	err := db.QueryRow("SELECT IMDb_id, Title, Year, Rating, Poster FROM movies WHERE IMDb_id = ?", imdbID).Scan(&imdb_id, &title, &year, &rating, &poster)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Movie not found")
+			return nil
+		}
+		return err
+	}
+	fmt.Printf("IMDb ID: %s\nTitle: %s\nYear: %d\nRating: %.1f\n", imdb_id, title, year, rating)
+	if poster.Valid {
+		fmt.Printf("Poster: %s\n", poster.String)
+	} else {
+		fmt.Println("Poster: null")
+	}
+
+	return nil
+}
+
+func deleteMovie(db *sql.DB, imdbID string) error {
+	stmt, err := db.Prepare("DELETE FROM movies WHERE IMDb_id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	res, err := stmt.Exec(imdbID)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		fmt.Println("No movie found with that IMDb ID")
+	} else {
+		fmt.Println("Movie deleted")
+	}
+
+	return nil
+}
 
 func main() {
 	arguments := os.Args[1:] // The first element is the path to the command, so we can skip that
@@ -20,17 +113,60 @@ func main() {
 	deleteCommand := flag.NewFlagSet("delete", flag.ExitOnError)
 	deleteImdbId := deleteCommand.String("imdbid", "tt0000001", "The IMDb ID of a movie or series")
 
+	if len(arguments) == 0 {
+		fmt.Println("Expected 'add', 'list', 'details' or 'delete' subcommands")
+		os.Exit(1)
+	}
+
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	switch arguments[0] {
 	case "add":
 		addCommand.Parse(arguments[1:])
-		// :)
+		if *addImdbId == "" || *addTitle == "" || *addYear == 0 || *addImdbRating == 0 {
+			fmt.Println("All fields (IMDb ID, Title, Year, Rating) are required")
+			os.Exit(1)
+		}
+		err := addMovie(db, *addImdbId, *addTitle, *addYear, *addImdbRating)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Movie added successfully")
+
 	case "list":
-		// :)
+		err := listMovies(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	case "details":
 		detailsCommand.Parse(arguments[1:])
-		// :)
+		if *detailsImdbId == "" {
+			fmt.Println("IMDb ID is required")
+			os.Exit(1)
+		}
+		err := showMovieDetails(db, *detailsImdbId)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	case "delete":
 		deleteCommand.Parse(arguments[1:])
-		// :)
+		if *deleteImdbId == "" {
+			fmt.Println("IMDb ID is required")
+			os.Exit(1)
+		}
+		err := deleteMovie(db, *deleteImdbId)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	default:
+		fmt.Println("Expected 'add', 'list', 'details' or 'delete' subcommands")
+		os.Exit(1)
 	}
 }
